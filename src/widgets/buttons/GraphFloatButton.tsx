c91124bcabf3b07510155158bede4e1f76f56ba5
Kaburi <specialemail90@gmail.com>
2024-09-11 11:59:14 +0900

    .

--- src/widgets/buttons/GraphFloatButton.tsx
+++ src/widgets/buttons/GraphFloatButton.tsx
@@ -9,9 +9,13 @@ export const GraphFloatButton = ({ onClick }: GraphFloatButtonProps) => {
     <button
       onClick={onClick}
       className={cn(
-        "fixed bottom-4 right-4 w-fit rounded-full border-2 border-blue-200 bg-blue-500 p-2 font-bold",
+        "fixed w-fit rounded-full border-2 border-blue-200 bg-blue-500 p-2 font-bold",
         "hover:cursor-pointer",
       )}
+      style={{
+        top: `calc(var(--vh) * 100 - 9.5rem)`,
+        right: `1rem`,
+      }}
     >
       <GoGraph className="text-white" />
     </button>
