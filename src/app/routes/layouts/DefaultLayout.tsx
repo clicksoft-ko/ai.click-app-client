2a8269f4a517de6fdc72ec408b85de5cf449099a
Kaburi <specialemail90@gmail.com>
2024-09-11 11:44:37 +0900

    .

--- src/app/routes/layouts/DefaultLayout.tsx
+++ src/app/routes/layouts/DefaultLayout.tsx
@@ -6,7 +6,7 @@ export const DefaultLayout = () => {
   useDvh();
 
   return (
-    <div className={cn("min-h-screen w-full overflow-hidden")}>
+    <div className={cn("h-[100vh] w-full overflow-hidden")}>
       <Outlet />
     </div>
   );
